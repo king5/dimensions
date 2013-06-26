94032f51348f168aa4064ceb81c5fac7c436402f
alexesba <alejandro.espinoza@tangosource.com>
2013-06-26 11:46:52 -0500

    start to migrate the search engine from searchify to elasticsearch

--- app/workers/entry_indexer.rb
+++ app/workers/entry_indexer.rb
@@ -3,7 +3,8 @@ class EntryIndexer
   @queue = :entries
 
   def self.perform(entry_id)
-    index = Dimensions::SearchifyApi.instance.indexes(APP_CONFIG['searchify_index'])
+    # index = Dimensions::SearchifyApi.instance.indexes(APP_CONFIG['searchify_index'])
+    index = Dimensions::ElasticsearchApi.instance.indexes(APP_CONFIG['searchify_index'])
 
     entry = FeedEntry.find(entry_id)
     entry.index_in_searchify(index)
